a78d1cfe2b74280cc876cbc48baee2a17718825d
TiroZit <nikus.master@gmail.com>
2021-12-09 17:21:19 +0500

    clipboard

--- src/js/components/clipboard.js
+++ src/js/components/clipboard.js
@@ -8,6 +8,5 @@ document.querySelector('.welcome__body').onclick = (event) => {
         document.querySelector('.welcome__btn').innerHTML = 'Скопировано';
         setTimeout(()=>document.querySelector('.welcome__btn').innerHTML='Начать игру'+'<svg class="arrow-right" style="width: 30px; height: 30px; fill: undefined; stroke: undefined;"><use xlink:href="img/sprite.svg#arrow-right"></use></svg>', 1000)
       })
-    console.log(value);
   }
 }
