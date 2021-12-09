32aa4c4b8f2fa53b8041cc93e5511a956db19951
TiroZit <nikus.master@gmail.com>
2021-12-09 17:32:38 +0500

    clipboard

--- src/js/components/clipboard.js
+++ src/js/components/clipboard.js
@@ -1,12 +1,12 @@
 document.querySelector('.welcome__body').onclick = (event) => {
   const elem = event.target;
-  if(elem.classList.contains('welcome__btn')){
+  if(elem.classList.contains('ip')){
     const ip = document.getElementById('ip');
     let value = ip.textContent;
     navigator.clipboard.writeText(value)
       .then(()=>{
-        document.querySelector('.welcome__btn').innerHTML = 'Скопировано';
-        setTimeout(()=>document.querySelector('.welcome__btn').innerHTML='Начать игру'+'<svg class="arrow-right" style="width: 30px; height: 30px; fill: undefined; stroke: undefined;"><use xlink:href="img/sprite.svg#arrow-right"></use></svg>', 1000)
+        document.querySelector('.ip').innerHTML = 'Скопировано';
+        setTimeout(()=>document.querySelector('.ip').innerHTML=value, 1500)
       })
   }
 }
