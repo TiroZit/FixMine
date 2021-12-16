19e59d41fc88a3d7e2f8b2dcab0c953318c0fe16
TiroZit <nikus.master@gmail.com>
2021-12-16 13:44:31 +0500

    new tabs

--- src/js/components/tab-1.js
+++ src/js/components/tab-1.js
@@ -1,127 +1,49 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//   tabbis();
-// });
-// (function($) {
-//   $.fn.multitabs = function() {
-//     var _this = $(this);
-//     var tabs = _this.children('.tab__header[data-child="false"]').children('button');
-//     var childrenTabs = _this.find('.tab__header[data-child="true"]').children('button');
+(function($) {
+  $.fn.multitabs = function() {
+    var _this = $(this);
+    var tabs = _this.children('.tab__header[data-child="false"]').children('button');
+    var childrenTabs = _this.find('.tab__header[data-child="true"]').children('button');
 
-//     $(tabs).add(childrenTabs).on('click', function() {
-//       var num;
-//       var _this = $(this);
-//       var classNameTab = _this.attr('class').split(' ');
-//       var classNameContent = _this.parent().siblings().children();
+    $(tabs).add(childrenTabs).on('click', function() {
+      var num;
+      var _this = $(this);
+      var classNameTab = _this.attr('class').split(' ');
+      var classNameContent = _this.parent().siblings().children();
 
-//       for (i = 0; i <= classNameTab.length; i++) {
-//         if (/([\d.]+)/.test(classNameTab[i])) {
-//           var isChild = $(_this).parent().data('child');
-//           num = classNameTab[i].split('-')[1];
+      for (i = 0; i <= classNameTab.length; i++) {
+        if (/([\d.]+)/.test(classNameTab[i])) {
+          var isChild = $(_this).parent().data('child');
+          num = classNameTab[i].split('-')[1];
           
-//           if (!isChild) {
-//             $(tabs).removeClass('active');
-//             $(_this).addClass('active');
-//           } else {
-//             $(_this).siblings().removeClass('active');
-//             $(_this).addClass('active');
-//           }
+          if (!isChild) {
+            $(tabs).removeClass('active');
+            $(_this).addClass('active');
+          } else {
+            $(_this).siblings().removeClass('active');
+            $(_this).addClass('active');
+          }
 
-//         }
-//       }
+        }
+      }
 
-//       $(classNameContent).each(function(i, n) {
-//         var name = $(n).attr('class').split(' ');
-//         for (n = 0; n <= name.length; n++) {
-//           if (name[n] != undefined) {
-//             if (/([\d.]+)/.test(name[n])) {
-//               if (num === name[n].split('-')[1]) {
-//                 var el = $(classNameContent)[num - 1];
-//                 $(classNameContent).removeClass('active');
-//                 $(el).addClass('active');
-//               }
-//             }
-//           }
-//         }
-//       });
+      $(classNameContent).each(function(i, n) {
+        var name = $(n).attr('class').split(' ');
+        for (n = 0; n <= name.length; n++) {
+          if (name[n] != undefined) {
+            if (/([\d.]+)/.test(name[n])) {
+              if (num === name[n].split('-')[1]) {
+                var el = $(classNameContent)[num - 1];
+                $(classNameContent).removeClass('active');
+                $(el).addClass('active');
+              }
+            }
+          }
+        }
+      });
 
-//     });
+    });
 
-//   }
-// })(jQuery);
+  }
+})(jQuery);
 
-// $('.tab1').multitabs();
-  document.querySelector('.donate').innerHTML +=`<div class=donate__container  hidden="" role="tabpanel" aria-labelledby="tab-0-2" id="tabpanel-0-2" tabindex="0">
-          <nav class=tabs__footer-items data-tabs><button
-              class=tabs__footer-link>Один Блок</button><button class=tabs__footer-link>БедВарс</button></nav>
-          <div class=donate__title-block>
-            <div class="o-block donate__block"></div>
-            <h2 class=donate__title>Покупка привилегий</h2>
-          </div>
-          <div class=donate__wrapper data-panes>
-            <div class=donate__items><a class=donate__item data-path=donate>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a><a class=donate__item data-path=donate>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a><a class=donate__item data-path=donate>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a></div>
-            <div class=donate__items><a class=donate__item>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a><a class=donate__item>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a></div>
-            <div class=donate__items><a class=donate__item>
-                <div class=donate__item-price>1000₽</div>
-                <div class=donate__item-content>
-                  <div class=donate__item-image><img src=img/donate/helper.webp alt=Helper>
-                    <div class=donate__item-title>Helper</div>
-                  </div>
-                  <div class=donate__item-cart><svg class=cart style="width: 30px; height: 30px; fill: ; stroke: #000;">
-                      <use xlink:href=img/sprite.svg#cart></use>
-                    </svg></div>
-                </div>
-              </a></div>
-          </div>
-        </div>
-`;
-  document.querySelector('.tabs__header-items').innerHTML += `<button class=tabs__header-link aria-selected="false" tabindex="-1" role="tab" aria-controls="tabpanel-0-2" id="tab-0-2">Привилегии</button>`;
+$('.tab1').multitabs();
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,261 +1,262 @@
-class tabbisClass {
-	init(options) {
-		this.thisOptions(options);
-		this.thisMemory();
-		this.setup();
-	}
+// class tabbisClass {
+// 	init(options) {
+// 		this.thisOptions(options);
+// 		this.thisMemory();
+// 		this.setup();
+// 	}
 
-	// Setup
-	setup() {
-		const panes = document.querySelectorAll(this.options.paneGroup);
-		const tabs = document.querySelectorAll(this.options.tabGroup);
+// 	// Setup
+// 	setup() {
+// 		const panes = document.querySelectorAll(this.options.paneGroup);
+// 		const tabs = document.querySelectorAll(this.options.tabGroup);
 
-		tabs.forEach((tabGroups, groupIndex) => {
-			const paneGroups = panes[groupIndex];
-			const activeIndex = this.getActiveIndex(tabGroups, groupIndex);
+// 		tabs.forEach((tabGroups, groupIndex) => {
+// 			const paneGroups = panes[groupIndex];
+// 			const activeIndex = this.getActiveIndex(tabGroups, groupIndex);
 
-			tabGroups.setAttribute('role', 'tablist');
+// 			tabGroups.setAttribute('role', 'tablist');
 
-			// Reset items
-			this.resetTabs([ ...tabGroups.children ]);
-			this.resetPanes([ ...paneGroups.children ]);
+// 			// Reset items
+// 			this.resetTabs([ ...tabGroups.children ]);
+// 			this.resetPanes([ ...paneGroups.children ]);
 
-			[ ...tabGroups.children ].forEach((tabItem, tabIndex) => {
-				const paneItem = paneGroups.children[tabIndex];
+// 			[ ...tabGroups.children ].forEach((tabItem, tabIndex) => {
+// 				const paneItem = paneGroups.children[tabIndex];
 
-				// Add attributes
-				this.addTabAttributes(tabItem, groupIndex);
-				this.addPaneAttributes(tabItem, paneItem);
+// 				// Add attributes
+// 				this.addTabAttributes(tabItem, groupIndex);
+// 				this.addPaneAttributes(tabItem, paneItem);
 
-				tabItem.groupIndex = groupIndex;
+// 				tabItem.groupIndex = groupIndex;
 
-				// Trigger event
-				tabItem.addEventListener(this.options.trigger, (e) => {
-					this.activate(e.currentTarget, tabItem.groupIndex);
-				});
+// 				// Trigger event
+// 				tabItem.addEventListener(this.options.trigger, (e) => {
+// 					this.activate(e.currentTarget, tabItem.groupIndex);
+// 				});
 
-				// Key event
-				if (this.options.keyboardNavigation) {
-					tabItem.addEventListener('keydown', (e) => {
-						this.eventKey(e);
-					});
-				}
-			});
+// 				// Key event
+// 				if (this.options.keyboardNavigation) {
+// 					tabItem.addEventListener('keydown', (e) => {
+// 						this.eventKey(e);
+// 					});
+// 				}
+// 			});
 
-			if (activeIndex !== null) {
-				this.activateTab([ ...tabGroups.children ][activeIndex]);
-				this.activatePane([ ...paneGroups.children ][activeIndex]);
-			}
-		});
-	}
+// 			if (activeIndex !== null) {
+// 				this.activateTab([ ...tabGroups.children ][activeIndex]);
+// 				this.activatePane([ ...paneGroups.children ][activeIndex]);
+// 			}
+// 		});
+// 	}
 
-	// Event key
-	eventKey(e) {
-		if ([ 13, 37, 38, 39, 40 ].includes(e.keyCode)) {
-			e.preventDefault();
-		}
+// 	// Event key
+// 	eventKey(e) {
+// 		if ([ 13, 37, 38, 39, 40 ].includes(e.keyCode)) {
+// 			e.preventDefault();
+// 		}
 
-		if (e.keyCode == 13) {
-			e.currentTarget.click();
-		} else if ([ 39, 40 ].includes(e.keyCode)) {
-			this.step(e, 1);
-		} else if ([ 37, 38 ].includes(e.keyCode)) {
-			this.step(e, -1);
-		}
-	}
+// 		if (e.keyCode == 13) {
+// 			e.currentTarget.click();
+// 		} else if ([ 39, 40 ].includes(e.keyCode)) {
+// 			this.step(e, 1);
+// 		} else if ([ 37, 38 ].includes(e.keyCode)) {
+// 			this.step(e, -1);
+// 		}
+// 	}
 
-	// Index
-	index(el) {
-		return [ ...el.parentElement.children ].indexOf(el);
-	}
+// 	// Index
+// 	index(el) {
+// 		return [ ...el.parentElement.children ].indexOf(el);
+// 	}
 
-	// Step
-	step(e, direction) {
-		const children = e.currentTarget.parentElement.children;
-		this.resetTabindex(children);
+// 	// Step
+// 	step(e, direction) {
+// 		const children = e.currentTarget.parentElement.children;
+// 		this.resetTabindex(children);
 
-		let el = children[this.pos(e.currentTarget, children, direction)];
-		el.focus();
-		el.setAttribute('tabindex', 0);
-	}
+// 		let el = children[this.pos(e.currentTarget, children, direction)];
+// 		el.focus();
+// 		el.setAttribute('tabindex', 0);
+// 	}
 
-	resetTabindex(children) {
-		[ ...children ].forEach((child) => {
-			child.setAttribute('tabindex', '-1');
-		});
-	}
+// 	resetTabindex(children) {
+// 		[ ...children ].forEach((child) => {
+// 			child.setAttribute('tabindex', '-1');
+// 		});
+// 	}
 
-	// Pos
-	pos(tab, children, direction) {
-		let pos = this.index(tab);
-		pos += direction;
+// 	// Pos
+// 	pos(tab, children, direction) {
+// 		let pos = this.index(tab);
+// 		pos += direction;
 
-		if (children.length <= pos) {
-			pos = 0;
-		} else if (pos == -1) {
-			pos = children.length - 1;
-		}
+// 		if (children.length <= pos) {
+// 			pos = 0;
+// 		} else if (pos == -1) {
+// 			pos = children.length - 1;
+// 		}
 
-		return pos;
-	}
+// 		return pos;
+// 	}
 
-	// Emit event
-	emitEvent(tab, pane) {
-		let event = new CustomEvent('tabbis', {
-			bubbles: true,
-			detail: {
-				tab: tab,
-				pane: pane
-			}
-		});
+// 	// Emit event
+// 	emitEvent(tab, pane) {
+// 		let event = new CustomEvent('tabbis', {
+// 			bubbles: true,
+// 			detail: {
+// 				tab: tab,
+// 				pane: pane
+// 			}
+// 		});
 
-		tab.dispatchEvent(event);
-	}
+// 		tab.dispatchEvent(event);
+// 	}
 
-	// Set active
-	getActiveIndex(groupTabs, groupIndex) {
-		const memory = this.loadMemory(groupIndex);
+// 	// Set active
+// 	getActiveIndex(groupTabs, groupIndex) {
+// 		const memory = this.loadMemory(groupIndex);
 
-		if (typeof memory !== 'undefined') {
-			return memory;
-		} else {
-			let element = groupTabs.querySelector(this.options.tabActive);
+// 		if (typeof memory !== 'undefined') {
+// 			return memory;
+// 		} else {
+// 			let element = groupTabs.querySelector(this.options.tabActive);
 
-			if (!element) {
-				element = groupTabs.querySelector('[aria-selected="true"]');
-			}
+// 			if (!element) {
+// 				element = groupTabs.querySelector('[aria-selected="true"]');
+// 			}
 
-			if (element) {
-				return this.index(element);
-			} else if (this.options.tabActiveFallback !== false) {
-				return this.options.tabActiveFallback;
-			} else {
-				return null;
-			}
-		}
-	}
+// 			if (element) {
+// 				return this.index(element);
+// 			} else if (this.options.tabActiveFallback !== false) {
+// 				return this.options.tabActiveFallback;
+// 			} else {
+// 				return null;
+// 			}
+// 		}
+// 	}
 
-	// ATTRIBUTES
+// 	// ATTRIBUTES
 
-	// Add tab attributes
-	addTabAttributes(tab, groupIndex) {
-		const tabIndex = this.index(tab);
-		const prefix = this.options.prefix;
+// 	// Add tab attributes
+// 	addTabAttributes(tab, groupIndex) {
+// 		const tabIndex = this.index(tab);
+// 		const prefix = this.options.prefix;
 
-		tab.setAttribute('role', 'tab');
-		tab.setAttribute('aria-controls', `${prefix}tabpanel-${groupIndex}-${tabIndex}`);
-		tab.setAttribute('id', `${prefix}tab-${groupIndex}-${tabIndex}`);
-	}
+// 		tab.setAttribute('role', 'tab');
+// 		tab.setAttribute('aria-controls', `${prefix}tabpanel-${groupIndex}-${tabIndex}`);
+// 		tab.setAttribute('id', `${prefix}tab-${groupIndex}-${tabIndex}`);
+// 	}
 
-	// Add tabpanel attributes
-	addPaneAttributes(tab, pane) {
-    console.log(tab);
-		pane.setAttribute('role', 'tabpanel');
-		pane.setAttribute('aria-labelledby', tab.getAttribute('id'));
-		pane.setAttribute('id', tab.getAttribute('aria-controls'));
-		pane.setAttribute('tabindex', '0');
-	}
+// 	// Add tabpanel attributes
+// 	addPaneAttributes(tab, pane) {
+//     //console.log(tab);
+// 		pane.setAttribute('role', 'tabpanel');
+// 		pane.setAttribute('aria-labelledby', tab.getAttribute('id'));
+// 		pane.setAttribute('id', tab.getAttribute('aria-controls'));
+// 		pane.setAttribute('tabindex', '0');
+// 	}
 
-	// Activate
-	activate(tab, i) {
-		const pane = document.querySelector(`#${tab.getAttribute('aria-controls')}`);
+// 	// Activate
+// 	activate(tab, i) {
+// 		const pane = document.querySelector(`#${tab.getAttribute('aria-controls')}`);
 
-		this.resetTabs([ ...tab.parentNode.children ]);
-		this.resetPanes([ ...pane.parentElement.children ]);
+// 		this.resetTabs([ ...tab.parentNode.children ]);
+// 		this.resetPanes([ ...pane.parentElement.children ]);
 
-		this.activateTab(tab);
-		this.activatePane(pane);
+// 		this.activateTab(tab);
+// 		this.activatePane(pane);
 
-		this.saveMemory(tab, i);
+// 		this.saveMemory(tab, i);
 
-		this.emitEvent(tab, pane);
-	}
+// 		this.emitEvent(tab, pane);
+// 	}
 
-	// Activate tab
-	activateTab(tab) {
-		tab.setAttribute('aria-selected', 'true');
-		tab.setAttribute('tabindex', '0');
-	}
+// 	// Activate tab
+// 	activateTab(tab) {
+// 		console.log('1');
+// 		tab.setAttribute('aria-selected', 'true');
+// 		tab.setAttribute('tabindex', '0');
+// 	}
 
-	// Activate pane
-	activatePane(pane) {
-		pane.removeAttribute('hidden');
-	}
+// 	// Activate pane
+// 	activatePane(pane) {
+// 		pane.removeAttribute('hidden');
+// 	}
 
-	// Remove tab attributes
-	resetTabs(tabs) {
-    console.log(tabs);
-		tabs.forEach((el) => el.setAttribute('aria-selected', 'false'));
-		this.resetTabindex(tabs);
-	}
+// 	// Remove tab attributes
+// 	resetTabs(tabs) {
+//     //console.log(tabs);
+// 		tabs.forEach((el) => el.setAttribute('aria-selected', 'false'));
+// 		this.resetTabindex(tabs);
+// 	}
 
-	// Reset pane attributes
-	resetPanes(panes) {
-		panes.forEach((el) => el.setAttribute('hidden', ''));
-	}
+// 	// Reset pane attributes
+// 	resetPanes(panes) {
+// 		panes.forEach((el) => el.setAttribute('hidden', ''));
+// 	}
 
-	// MEMORY
+// 	// MEMORY
 
-	// Load memory
-	loadMemory(groupIndex) {
-		if (!this.options.memory) return;
-		if (typeof this.memory[groupIndex] === 'undefined') return;
-		if (this.memory[groupIndex] === null) return;
+// 	// Load memory
+// 	loadMemory(groupIndex) {
+// 		if (!this.options.memory) return;
+// 		if (typeof this.memory[groupIndex] === 'undefined') return;
+// 		if (this.memory[groupIndex] === null) return;
 
-		return parseInt(this.memory[groupIndex]);
-	}
+// 		return parseInt(this.memory[groupIndex]);
+// 	}
 
-	// Save memory
-	saveMemory(tab, groupIndex) {
-		if (!this.options.memory) return;
-		this.memory[groupIndex] = this.index(tab);
-		localStorage.setItem(this.options.memory, JSON.stringify(this.memory));
-	}
+// 	// Save memory
+// 	saveMemory(tab, groupIndex) {
+// 		if (!this.options.memory) return;
+// 		this.memory[groupIndex] = this.index(tab);
+// 		localStorage.setItem(this.options.memory, JSON.stringify(this.memory));
+// 	}
 
-	// This memory
-	thisMemory() {
-		if (!this.options.memory) return;
-		const store = localStorage.getItem(this.options.memory);
-		this.memory = store !== null ? JSON.parse(store) : [];
-	}
+// 	// This memory
+// 	thisMemory() {
+// 		if (!this.options.memory) return;
+// 		const store = localStorage.getItem(this.options.memory);
+// 		this.memory = store !== null ? JSON.parse(store) : [];
+// 	}
 
-	// OPTIONS
+// 	// OPTIONS
 
-	// Defaults
-	defaults() {
-		return {
-			keyboardNavigation: true,
-			memory: false,
-			paneGroup: '[data-panes]',
-			prefix: '',
-			tabActive: '[data-active]',
-			tabActiveFallback: 0,
-			tabGroup: '[data-tabs]',
-			trigger: 'click'
-		};
-	}
+// 	// Defaults
+// 	defaults() {
+// 		return {
+// 			keyboardNavigation: true,
+// 			memory: false,
+// 			paneGroup: '[data-panes]',
+// 			prefix: '',
+// 			tabActive: '[data-active]',
+// 			tabActiveFallback: 0,
+// 			tabGroup: '[data-tabs]',
+// 			trigger: 'click'
+// 		};
+// 	}
 
-	// This options
-	thisOptions(options) {
-		this.options = Object.assign(this.defaults(), options);
-		if (this.options.memory !== true) return;
-		this.options.memory = 'tabbis';
-	}
-}
+// 	// This options
+// 	thisOptions(options) {
+// 		this.options = Object.assign(this.defaults(), options);
+// 		if (this.options.memory !== true) return;
+// 		this.options.memory = 'tabbis';
+// 	}
+// }
 
-// Function call
-function tabbis(options = {}) {
-	const tabs = new tabbisClass();
-	tabs.init(options);
-}
+// // Function call
+// function tabbis(options = {}) {
+// 	const tabs = new tabbisClass();
+// 	tabs.init(options);
+// }
 
-tabbis({
-  keyboardNavigation: true,
-  memory: false,
-  paneGroup: '[data-panes]',
-  prefix: '',
-  tabActive: '[data-active]',
-  tabActiveFallback: 0,
-  tabGroup: '[data-tabs]',
-  trigger: 'click'
-});
+// tabbis({
+//   keyboardNavigation: true,
+//   memory: false,
+//   paneGroup: '[data-panes]',
+//   prefix: '',
+//   tabActive: '[data-active]',
+//   tabActiveFallback: 0,
+//   tabGroup: '[data-tabs]',
+//   trigger: 'click'
+// });
